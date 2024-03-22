b4f4b870286461c9376b709f77b6368c5390e6b4
Anshuman1609 <anshumandubey1609@gmail.com>
2024-03-22 18:38:53 +0530

    NotFOund page setup

--- src/components/Notfound.js
+++ src/components/Notfound.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const Notfound = () => {
   return (
-    <div>Notfound</div>
+    <>
+      <div className="container text-center" style={{ position: 'absolute', left: 0, right: 0 }}>
+        <h1 className="container my-5">NOT FOUND</h1>
+        <h1 className="container my-3">Error 404</h1>
+        <Link to="/">
+          <button className="btn btn-primary  my-5 py-5" style={{ position: 'absolute', left: 0, right: 0 }}>Back to HomePage</button>
+        </Link>
+      </div>
+    </>
   )
 }
 
